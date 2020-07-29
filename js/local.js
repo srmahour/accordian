88d55d59bdea6dfff2b69dac605b49d07009ba65
srmahour <sadeepmahour@gmail.com>
2020-07-29 13:14:42 +0530

    accordian

---
+++ js/local.js
@@ -0,0 +1,17 @@
+  $(document).ready(function(){
+
+      $('.offerItemTitle').click(function(){
+
+        $(this).parents('.offerslide').children('.offerItem').removeClass('active');
+        $(this).parents('.offerslide').children('.offerItem').children('.offerItemTitle').removeClass('hide');
+        $(this).parent('.offerItem').addClass('active');
+        $(this).addClass('hide');
+
+      });
+
+
+
+  });
+
+
+
